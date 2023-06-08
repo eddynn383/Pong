cd5fb12fe1310ee8b9817d629f25a3a88b142b3c
EDC <85519028+eddynn383@users.noreply.github.com>
2023-06-08 10:09:27 +0300

    clear the code

--- Ball.js
+++ Ball.js
@@ -68,10 +68,6 @@ function randomNumberBetween(min, max) {
 }
 
 function isCollision(rect1, rect2) {
-    console.log(            rect1.left <= rect2.right &&
-        rect1.right >= rect2.left &&
-        rect1.top <= rect2.bottom &&
-        rect1.bottom >= rect2.top)
     return (
             rect1.left <= rect2.right &&
             rect1.right >= rect2.left &&
